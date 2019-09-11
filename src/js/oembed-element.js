57c3be337eae7c54b1625f869dca3efb942c3d8c
Dillon Kearns <dillon@dillonkearns.com>
2019-09-10 19:43:10 -0700

    Handle video types.

--- src/js/oembed-element.js
+++ src/js/oembed-element.js
@@ -33,14 +33,10 @@ customElements.define(
 
 
 
+      let iframe;
       switch (response.type) {
         case 'rich':
-        let iframe = document.createElement("iframe");
-        iframe.setAttribute('border', '0')
-        iframe.setAttribute('frameborder', '0')
-        iframe.setAttribute('height', response.height || 500)
-        iframe.setAttribute('width', response.width || 500)
-        iframe.srcdoc = response.html;
+        iframe = createIframe(response);
         shadow.appendChild(iframe);
 
         setTimeout(() => {
@@ -52,12 +48,23 @@ customElements.define(
           }
         }, 1000)
           break;
+        case 'video':
+          iframe = createIframe(response)
+          shadow.appendChild(iframe);
+
+          setTimeout(() => {
+            if (!response.height) {
+              // console.log('VIDEO', iframe.contentWindow.document.body.scrollHeight + 110)
+              shadow.querySelector('iframe').setAttribute('height', iframe.contentWindow.document.body.scrollHeight + 10)
+            }
+            if (!response.width) {
+              shadow.querySelector('iframe').setAttribute('width', iframe.contentWindow.document.body.scrollWidth + 10)
+            }
+          }, 1000)
+          break;
         case 'photo':
-          console.log('PHOTO!', response);
           let img = document.createElement("img");
           img.setAttribute('src', response.url)
-          // img.setAttribute('max-width', maxwidth)
-          // img.setAttribute('max-height', maxheight)
           img.setAttribute('style', `max-width: ${maxwidth}px; max-height: ${maxheight}px;`)
           shadow.appendChild(img)
         break;
@@ -70,3 +77,13 @@ customElements.define(
     }
   }
 );
+
+function createIframe(response) {
+  let iframe = document.createElement("iframe");
+  iframe.setAttribute('border', '0')
+  iframe.setAttribute('frameborder', '0')
+  iframe.setAttribute('height', (response.height || 500) + 20)
+  iframe.setAttribute('width', (response.width || 500) + 20)
+  iframe.srcdoc = response.html;
+  return iframe;
+}
